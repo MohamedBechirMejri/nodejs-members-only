4ae65b7b493f6c23e0a1112f50dba7356d197b3d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-02 18:51:18 +0100

    add signup route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
 });
+router.get("/signup", (req, res, next) => {
+  res.render("signup", { title: "Signup" });
+});
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "Login" });
+});
+// router.get("/logout", (req, res, next) => {
+//   res.render("index", { title: "Express" });
+// });
 
 module.exports = router;
