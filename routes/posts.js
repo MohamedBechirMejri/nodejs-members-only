36e055bc14d84f916a69dd2910fefb9f13da2c25
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-03 13:19:05 +0100

    add posts index

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
-  res.send("respond with a resource");
+  res.render("posts/index", { title: "Home" });
 });
 
 module.exports = router;
