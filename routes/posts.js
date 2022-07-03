aa596ef22b8357a0695bafeca5006062f0a0ff76
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-03 18:59:53 +0100

    add new post method

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,8 @@
+/* eslint-disable consistent-return */
 const express = require("express");
+const { body, validationResult } = require("express-validator");
+
+const Post = require("../models/Post");
 
 const router = express.Router();
 
@@ -15,9 +19,28 @@ router.get("/new", (req, res, next) => {
   }
 });
 
-router.post("/new", (req, res, next) => {
-  res.redirect("/");
-});
+router.post("/new", [
+  body("title", "Title is required").trim().escape().not().isEmpty(),
+  body("content", "Body is required").trim().escape().not().isEmpty(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("posts/new", {
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      user: req.user._id,
+    });
+    post.save(err => {
+      if (err) return next(err);
+      res.redirect("/posts");
+    });
+  },
+]);
 
 router.get("/:id/delete", (req, res, next) => {
   res.redirect("/");
