c1eb1e5c69934fd9cc7032029ad62a8b9a7f6b2a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-03 12:43:40 +0100

    redirect home if logged in

--- routes/index.js
+++ routes/index.js
@@ -14,24 +14,29 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-router.get("/signup", (req, res) => res.render("signup"));
+router.get("/signup", (req, res) => {
+  if (req.isAuthenticated()) res.redirect("/");
+  else res.render("signup");
+});
 
 router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    if (err) return next(err);
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword,
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      membership: "free",
-    });
-    user.save(err => {
+  if (req.isAuthenticated()) res.redirect("/");
+  else
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
       if (err) return next(err);
-      res.redirect("/");
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        membership: "free",
+      });
+      user.save(err => {
+        if (err) return next(err);
+        res.redirect("/");
+      });
     });
-  });
 });
 
 router.get("/login", (req, res) => {
